c94871e2096ec7b3dd210a73b81a433051612034
histnat <histoire.naturelle@hotmail.fr>
2016-03-02 10:27:48 +0100

    premier commit

---
+++ sources/mithraLib/trunk/src/main/java/org/mithralib/config/JSFConfig.java
@@ -0,0 +1,45 @@
+package org.mithralib.config;
+
+import javax.faces.webapp.FacesServlet;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages={"org.mithralib.*"})
+@Import(value = { LoginSecurityConfig.class })
+@EnableAutoConfiguration
+public class JSFConfig extends SpringBootServletInitializer {
+
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JSFConfig.class);
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        FacesServlet servlet = new FacesServlet();
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(servlet, "*.jsf");
+		return servletRegistrationBean;
+    }
+    
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
---
+++ sources/mithraLib/trunk/src/main/java/org/mithralib/config/core/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package org.mithralib.config.core;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+}
